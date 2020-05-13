fc35031ac7029585cdbaa26a92da6c59aae712d0
abanglaile <abanglaile@users.noreply.github.com>
2020-05-13 18:33:17 +0800

    上传lib文件

--- src/Reducer/auth_data.js
+++ src/Reducer/auth_data.js
@@ -8,11 +8,12 @@ const defaulatAuthData = Immutable.fromJS({
         realname:null,
         username: 'fei',
         // userid: 'aeb58cd002c411e98175d1610e288342',//欧老师
-        userid: '1927560040d911e9ad2ca1607a4b5d90',//Erica
+        // userid: '1927560040d911e9ad2ca1607a4b5d90',//Erica
         // userid:'33525d00037a11e9b965fd02f0885d74',//雪颖
         // userid:'3044f0f040ba11e9ad2ca1607a4b5d90',
         // userid:'59c6b18020d811ea9c37df76154ba416',
         // userid: 1,
+        userid: 'fef7046048c511ea95874dd75a6750f4',//喆
         isAuthenticated: false,
         isAuthenticating: false,
         statusText: null
